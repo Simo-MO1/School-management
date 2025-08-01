de18155d487fc01340fe0d9bbb20bba3b4f0691f
Simo-MO1 <alalisimo2005@gmail.com>
2025-08-02 00:51:31 +0200

    update

--- grade.cpp
+++ grade.cpp
@@ -119,3 +119,113 @@ std::vector<Grades> Grades::loadGradesFromFile(int gID)
   file.close();
   return result; // why??
 }
+
+void Grades::editGrade(int ID, const std::string& Subject){
+    std:: ifstream ginFile("grades.txt");
+    std:: vector<Grades>allGrades; //why this
+    std:: string line;
+
+    if(!ginFile.is_open()){
+      std::cerr<<"ERROR! Unable to open the file\n";
+      return;
+    }
+
+    while(std::getline(ginFile, line)){
+      std:: stringstream ss(line);
+      std:: string idStr, subtStr, markStr;
+
+      std::getline(ss, idStr, ',');
+      std::getline(ss, subtStr, ',');
+
+      int ID = stoi(idStr); //why we do this
+
+      std::vector<float>scores;
+
+      while(getline(ss, markStr, ',')){ //why this 
+        try{
+          scores.push_back(stof(markStr)); //whyyyy how could it modified it, while just adding extra instance
+        } catch(const std:: invalid_argument &e){std::cerr<<"Error! in line: "<<line<<e.what();}
+      }
+      allGrades.push_back(Grades(ID, Subject, scores)); //whyyyyyy no idea
+    }
+    ginFile.close();
+
+    bool found=false; //need to be clarified
+    for(Grades& g: allGrades){
+      if(g.getID()==ID && g.getSubject()==Subject){
+        found=true;
+        std::cout<<"Editing the grades for subject: '"<<Subject<<"'.\n";
+        std::cout<<"how many new grades?\n";
+        int count;
+        std::cin>>count;
+
+        std::vector<float>newScores; //why another vector
+        std::cout<<"Enter the new grade: \n";
+
+        for(int i=0;i<count;++i){
+          float score;
+          std::cin>>score;
+          newScores.push_back(score); //need to be clarifieeed
+        }
+        g=Grades(ID, Subject, newScores); //this one too
+        break;
+      }
+    }
+    if(!found){
+      std::cout<<"No grade found for the entered ID: "<<ID<<"and subject: "<<Subject<<std::endl;
+    }
+
+    std::ofstream goutFile("grades.txt");
+    for(const Grades& g: allGrades){     //this one too need to be clarified
+      goutFile<<g.getID()<<","<<g.getSubject();
+      for(float score: g.getScores())
+        goutFile<<","<<score;
+      goutFile.close();
+    }
+
+    std::cout<<"Grades updated successfully!\n";
+}
+
+void Grades::deleteGrade(int studentID, const std::string& Subject){
+  std:: ifstream ginFile("grades.txt");
+    std:: vector<Grades>allGrades; 
+    std:: string line;
+
+    if(!ginFile.is_open()){
+      std::cerr<<"ERROR! Unable to open the file\n";
+      return;
+    }
+
+while(std::getline(ginFile, line)){
+      std:: stringstream ss(line);
+      std:: string idStr, subtStr, markStr;
+
+      std::getline(ss, idStr, ',');
+      std::getline(ss, subtStr, ',');
+
+      int ID = stoi(idStr); //why we do this
+
+      std::vector<float>scores; //why another vector
+
+      while(getline(ss, markStr, ',')){ //why this 
+        try{
+          scores.push_back(stof(markStr)); //whyyyy how could it modified it, while just adding extra instance
+        } catch(const std:: invalid_argument &e){std::cerr<<"Error! in line: "<<line<<e.what();}
+      }
+    
+      if(!(ID==studentID && subtStr==Subject)){
+        allGrades.push_back(Grades(ID, Subject, scores)); //only grades that aren't entered will not be delete it or what?
+      }
+}
+ginFile.close();
+
+std::ofstream goutFile("grades.txt");
+ for(const Grades& g: allGrades){     //this one too need to be clarified
+      goutFile<<g.getID()<<","<<g.getSubject();
+      for(float score: g.getScores())
+        goutFile<<","<<score;
+      goutFile.close();
+    }
+
+ std::cout<<"Grades deleted successfully1\n";
+}
